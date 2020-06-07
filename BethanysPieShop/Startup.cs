6b36cb766a3799e9d36c61d1c774fd500c15cae2
tmwalo <vuyotm@gmail.com>
2020-06-07 17:46:25 +0200

    change: repository instances now use EF Core database

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -28,8 +28,8 @@ namespace BethanysPieShop
         {
             services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddScoped<IPieRepository, MockPieRepository>();
-            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+            services.AddScoped<IPieRepository, PieRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddControllersWithViews();
         }
 
