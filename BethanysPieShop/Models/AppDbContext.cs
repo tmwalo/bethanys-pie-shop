00cf29b444342122548b6ea9a768bc2a9b2e3bee
tmwalo <vuyotm@gmail.com>
2020-06-14 10:18:38 +0200

    add: shopping cart item migration

--- BethanysPieShop/Models/AppDbContext.cs
+++ BethanysPieShop/Models/AppDbContext.cs
@@ -17,6 +17,8 @@ namespace BethanysPieShop.Models
 
         public DbSet<Category> Categories { get; set; }
 
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
