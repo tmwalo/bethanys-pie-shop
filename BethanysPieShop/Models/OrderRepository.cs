05a36e4005645203f5a8120fd99b9e494bbf5ffd
tmwalo <vuyotm@gmail.com>
2020-06-16 11:21:12 +0200

    add: OrderRepository

---
+++ BethanysPieShop/Models/OrderRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrderRepository(AppDbContext appDbContext, ShoppingCart shoppingCart)
+        {
+            _appDbContext = appDbContext;
+            _shoppingCart = shoppingCart;
+        }
+
+        public void CreateOrder(Order order)
+        {
+            order.OrderPlaced = DateTime.Now;
+
+            _appDbContext.Orders.Add(order);
+
+            var shoppingCartItems = _shoppingCart.GetShoppingCartItems();
+
+            foreach (var shoppingCartItem in shoppingCartItems)
+            {
+                var orderDetail = new OrderDetail()
+                {
+                    OrderId = order.OrderId,
+                    PieId = shoppingCartItem.Pie.PieId,
+                    Amount = shoppingCartItem.Amount,
+                    Price = shoppingCartItem.Pie.Price
+                };
+
+                _appDbContext.OrderDetails.Add(orderDetail);
+            }
+
+            _appDbContext.SaveChanges();
+        }
+    }
+}
