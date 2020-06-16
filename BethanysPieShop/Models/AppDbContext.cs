def40f7fd8f85123aa2c79070d1d981a3787787b
tmwalo <vuyotm@gmail.com>
2020-06-16 10:56:01 +0200

    add: order and order details db sets

--- BethanysPieShop/Models/AppDbContext.cs
+++ BethanysPieShop/Models/AppDbContext.cs
@@ -19,6 +19,10 @@ namespace BethanysPieShop.Models
 
         public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
 
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
